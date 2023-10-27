003d1ea205e5d3f4b212eeb84d1a344a2716d151
Qde13 <nzemka0@gmail.com>
2023-10-27 19:20:46 +0300

    Add "Success" alert after submitting data

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from  datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -34,6 +34,7 @@ def index():
                     date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        flash(f"{first_name}, your form was submitted successfully!", "success")
 
     return render_template("index.html")
 
